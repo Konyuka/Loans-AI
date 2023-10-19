aa32c355bc8d923fed0193bb84ffaab5a1af474e
Konyuka <michaelsaiba84@gmail.com>
2023-10-19 11:40:05 +0300

    done

--- src/components/ProfileView.vue
+++ src/components/ProfileView.vue
@@ -485,7 +485,7 @@ onMounted(() => {
                                 </div>
                             </div>
                             <div class="mt-5 sm:mt-6">
-                                <button type="button" @click="summaryModal=summaryModal"
+                                <button type="button" @click="summaryModal=!summaryModal"
                                     class="inline-flex w-full justify-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                     Close Summary
                                 </button>
